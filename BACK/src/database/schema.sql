efa0f9a4b961242c744be6d46de9946300ef6155
Anna Beatriz <anna.b.valentim@aluno.senai.br>
2025-03-18 08:59:58 -0300

    Update schema.sql

--- BACK/src/database/schema.sql
+++ BACK/src/database/schema.sql
@@ -1,3 +1,31 @@
 CREATE DATABASE cadastro18;
 
 \c cadastro18;
+
+CREATE TABLE ingressos (
+    id SERIAL PRIMARY KEY,
+    evento VARCHAR(255) NOT NULL,
+    local VARCHAR(255) NOT NULL,
+    data_evento DATE NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade_disponivel INTEGER NOT NULL
+);
+
+CREATE TABLE ingressos_categorias (
+    id SERIAL PRIMARY KEY,
+    categoria VARCHAR(50) NOT NULL
+);
+
+INSERT INTO ingressos (evento, local, data_evento, categoria, preco, quantidade_disponivel) VALUES
+('Tour Morada 25', 'Campinas', 2025-03-22, 'VIP', R$200.00, 10),
+('Tour Morada 25', 'Florianópolis', 2025-04-26, 'Pista', R$100.00, 50),
+('Tour Morada 25', 'Florianópolis', 2025-04-26, 'Camarote', R$300.00, 100),
+('Tour Morada 25', 'Florianópolis', 2025-05-02, 'VIP', R$200.00, 10),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'Arquibancada', R$80.00, 100),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'Seguidores do Morada no YouTube', R$65.00, 250),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'Inscritos no canal do Morada no YouTube', R$180.00, 10),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'VIP', R$.200, 25),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'Pista', R$100.00, 50),
+('Tour Morada 25', 'São Paulo', 2025-05-02, 'Camarote', R$300.00, 100),
+('Tour Morada 25', 'Rio de Janeiro', 2025-05-02, 'VIP', R$200.00, 0);
